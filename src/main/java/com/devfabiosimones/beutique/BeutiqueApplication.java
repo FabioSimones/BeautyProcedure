99a68902b6afe9eaac214cddb65e85f2ca8af667
FabioSimones <simones.fabio1994@gmail.com>
2026-01-06 19:07:23 -0300

    Criando o projeto.

---
+++ src/main/java/com/devfabiosimones/beutique/BeutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.beutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeutiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfabiosimones/beutique/BeutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.beutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
